982555a8ef12200ae755c7352ddb788b761367a8
yadavpurva <yadav.purva2429@gmail.com>
2024-12-18 15:19:53 +0530

    testing

---
+++ src/main/java/com/validationlogin/userController/UserController.java
@@ -0,0 +1,28 @@
+package com.gc;
+
+import com.validationlogin.entities.User;
+import com.validationlogin.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+@RestController
+*/
+public class UserController {
+
+   /* @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public List<User> getUser(){
+        System.out.println("getting Users");
+        return this.userService.getUsers();
+    }*/
+
+    }
+
+
+
+
